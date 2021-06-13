8dff3a8cfafa7ef5000e7c936909eb5e9dc0af58
Farhad Uneci <farhaduneci@gmail.com>
2021-06-13 21:47:43 +0430

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './index.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      'main': ['Inter', 'sans-serif']
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
